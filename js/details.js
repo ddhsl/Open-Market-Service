7ce43d79e106f6581ce44952811cd1e72764122b
ddhsl <dnjs9199@naver.com>
2024-10-17 15:49:21 +0900

    class 및 로그인 경로 추가

--- js/details.js
+++ js/details.js
@@ -1,5 +1,10 @@
 const logo = document.querySelector('.logo');
+const login = document.querySelector('.login');
 
 logo.addEventListener('click', () => {
     window.location.href = '/pages/product_list.html';
+});
+
+login.addEventListener('click', () => {
+    window.location.href = '/pages/login.html';
 });
