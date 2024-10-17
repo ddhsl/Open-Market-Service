84722558754dd63ebf69e76784009cd8b030ffed
ddhsl <dnjs9199@naver.com>
2024-10-17 15:48:57 +0900

    logo 경로 추가

--- js/list.js
+++ js/list.js
@@ -1,5 +1,10 @@
+const logo = document.querySelector('.logo');
 const loginBtn = document.querySelector('.login');
-const blanket = document.querySelector('.blanket')
+const blanket = document.querySelector('.blanket');
+
+logo.addEventListener('click', () => {
+    window.location.href = '/pages/product_list.html';
+});
 
 loginBtn.addEventListener('click', () => {
     window.location.href = '/pages/login.html'; 
