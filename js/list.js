15f344fcc3e9b104359b8c8fc46c7e14dc7a26a0
ddhsl <dnjs9199@naver.com>
2024-10-17 15:34:18 +0900

    상품페이지 경로 추가

--- js/list.js
+++ js/list.js
@@ -1,5 +1,10 @@
 const loginBtn = document.querySelector('.login');
+const blanket = document.querySelector('.blanket')
 
 loginBtn.addEventListener('click', () => {
     window.location.href = '/pages/login.html'; 
 });
+
+blanket.addEventListener('click', () => {
+    window.location.href = '/pages/product_details.html';
+})
